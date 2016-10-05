1c1ac606d83d31012a83f1ab41187013d350f122
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2016-10-05 16:36:11 +0200

    Pas de notice sur les medias si media not found comme ça.

--- src/Medias/Medias.php
+++ src/Medias/Medias.php
@@ -13,8 +13,9 @@ class Medias {
     public static function mediaAtSize($mediaUrl,$size, $icon=false, $attr=''){
         global $wpdb;
         //Url to ID
-        $attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid='%s';", $mediaUrl ));
-        return wp_get_attachment_image($attachment[0], $size, $icon, $attr);
+        $attachmentCol = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid='%s';", $mediaUrl ));
+        $imgId = !empty($attachmentCol[0]) ? $attachmentCol[0] : 0;
+        return wp_get_attachment_image($imgId, $size, $icon, $attr);
     }
 
 }
