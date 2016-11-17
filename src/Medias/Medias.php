b3314c0ac2dd8aa48d4472218a38e509de37bb7a
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2016-11-17 19:16:19 +0100

    deported method Medias::getIdFormUrl

--- src/Medias/Medias.php
+++ src/Medias/Medias.php
@@ -10,11 +10,16 @@ namespace WonderWp\Medias;
 
 class Medias {
 
-    public static function mediaAtSize($mediaUrl,$size, $icon=false, $attr=''){
+    public static function getIdFromUrl($mediaUrl){
         global $wpdb;
         //Url to ID
         $attachmentCol = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid='%s';", $mediaUrl ));
         $imgId = !empty($attachmentCol[0]) ? $attachmentCol[0] : 0;
+        return $imgId;
+    }
+
+    public static function mediaAtSize($mediaUrl,$size, $icon=false, $attr=''){
+        $imgId = self::getIdFromUrl($mediaUrl);
         return wp_get_attachment_image($imgId, $size, $icon, $attr);
     }
 
