d01959cba465279e3a620b5285d02c80f1b623ee
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2017-01-25 17:25:42 +0100

    fix passage des liens en relatif pr assets

--- src/Assets/JsonAssetEnqueuer.php
+++ src/Assets/JsonAssetEnqueuer.php
@@ -18,7 +18,7 @@ class JsonAssetEnqueuer extends AbstractAssetEnqueuer{
     {
         parent::__construct();
         $this->_manifest = json_decode(file_get_contents($manifestPath));
-        $this->_blogurl = rtrim("//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}",'/');
+        $this->_blogurl = rtrim("//{$_SERVER['HTTP_HOST']}",'/');
         $this->version = 0;
     }
 
