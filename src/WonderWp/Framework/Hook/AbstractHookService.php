005f3aca778bff48433103cd579ab8f07f77b1f8
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2017-04-06 16:43:36 +0200

    loadTextdomain in abstract hook service

--- src/WonderWp/Framework/Hook/AbstractHookService.php
+++ src/WonderWp/Framework/Hook/AbstractHookService.php
@@ -2,9 +2,30 @@
 
 namespace WonderWp\Framework\Hook;
 
+use WonderWp\Framework\AbstractPlugin\AbstractManager;
 use WonderWp\Framework\Service\AbstractService;
 
 abstract class AbstractHookService extends AbstractService implements HookServiceInterface
 {
+
+    /**
+     * @var AbstractManager
+     */
+    protected $manager;
+
+    /**
+     * Load Textdomain
+     */
+    public function loadTextdomain()
+    {
+        $domain = $this->manager->getConfig('textDomain');
+        $locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+        $languageDir = $this->manager->getConfig('path.base') . '/languages/';
+
+        // wp-content/languages/plugin-name/plugin-name-de_DE.mo
+        load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '-' . $locale . '.mo' );
+        // wp-content/plugins/plugin-name/languages/plugin-name-de_DE.mo
+        load_plugin_textdomain( $domain, FALSE, $languageDir );
+    }
 
 }
