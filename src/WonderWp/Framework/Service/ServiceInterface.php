48f6b71db9904fa1fabc5a0090743acca00a6f6c
Cyril SAINT-YVES <cyril.saint-yves@triotech.fr>
2017-04-07 18:46:06 +0200

    (De)Activator are now services !

--- src/WonderWp/Framework/Service/ServiceInterface.php
+++ src/WonderWp/Framework/Service/ServiceInterface.php
@@ -4,13 +4,15 @@ namespace WonderWp\Framework\Service;
 
 interface ServiceInterface
 {
+    const ACTIVATOR_NAME          = 'activator';
+    const API_SERVICE_NAME        = 'api';
     const ASSETS_SERVICE_NAME     = 'assets';
+    const COMMAND_SERVICE_NAME    = 'command';
+    const DEACTIVATOR_NAME        = 'deactivator';
     const HOOK_SERVICE_NAME       = 'hooks';
-    const ROUTE_SERVICE_NAME      = 'route';
     const LIST_TABLE_SERVICE_NAME = 'listTable';
     const MODEL_FORM_SERVICE_NAME = 'modelForm';
-    const COMMAND_SERVICE_NAME    = 'command';
-    const VIEW_SERVICE_NAME       = 'view';
-    const API_SERVICE_NAME        = 'api';
+    const ROUTE_SERVICE_NAME      = 'route';
     const SHORT_CODE_SERVICE_NAME = 'shortCode';
+    const VIEW_SERVICE_NAME       = 'view';
 }
