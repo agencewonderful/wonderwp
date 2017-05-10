985cd0207c9f9edc894ae67202868bb3e38a943f
monsieurluge <monsieurluge@gmail.com>
2017-05-10 18:53:43 +0200

    fix: RawCategoryField devient CategoryField

--- src/WonderWp/Framework/Form/Field/CategoryField.php
+++ src/WonderWp/Framework/Form/Field/CategoryField.php
@@ -2,53 +2,24 @@
 
 namespace WonderWp\Framework\Form\Field;
 
-// @deprecated
-class CategoryField extends SelectField
-{
-    /** @inheritdoc */
-    public function __construct($name, $value = null, array $displayRules = [], array $validationRules = [], $parent = 0)
-    {
-        parent::__construct($name, $value, $displayRules, $validationRules);
+use WonderWp\Framework\Form\Field\AbstractCategoryField;
 
-        $this->setCatOptions($parent);
-    }
+class CategoryField extends AbstractCategoryField
+{
 
     /**
-     * @param integer $parent
-     *
-     * @return static
+     * @inheritDoc
      */
-    public function setCatOptions($parent)
+    public function doSetOptions()
     {
-        $displayRules = $this->getDisplayRules();
-        $firstLabel   = (!empty($displayRules) && !empty($displayRules['label'])) ? $displayRules['label'] : __('Category', $this->getTextDomain());
-        $options      = [
-            '' => $firstLabel,
-        ];
-
-        $args = [
-            'child_of'   => $parent,
-            'hide_empty' => false,
-        ];
-        $cats = get_categories($args);
-
-        if (!empty($cats)) {
-            foreach ($cats as $cat) {
-                /** @var $cat \WP_Term */
-                $options[$cat->term_id] = __('term_' . $cat->slug, $this->getTextDomain());
-            }
+        foreach ($this->categories as $category) {
+            /** @var \WP_Term $category */
+            $this->selectOptions[$category->term_id] = $category->name;
         }
 
-        $this->setOptions($options);
+        $this->setOptions($this->selectOptions);
 
         return $this;
     }
 
-    /**
-     * @return string
-     */
-    protected function getTextDomain()
-    {
-        return defined('WWP_THEME_TEXTDOMAIN') ? WWP_THEME_TEXTDOMAIN : 'default';
-    }
 }
